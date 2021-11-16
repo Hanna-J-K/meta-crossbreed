aa84ecbd0d1a647501bdc8106da8de2dc2925500
Mateusz Roszkowski <blackspider1@o2.pl>
2021-11-16 18:14:49 +0100

    I love string split
    Co-authored-by: Hanna-J-K <Hanna-J-K@users.noreply.github.com>

---
+++ help.py
@@ -0,0 +1,37 @@
+xd = '''1 	Toporek 	32 252 	68 674
+2 	Moneta z brazu 	225 790 	471 010
+3 	Korona 	468 164 	944 620
+4 	Diamentowy posazek 	489 494 	962 094
+5 	Szmaragdowy pas 	35 384 	78 344
+6 	Skamieliny 	265 590 	579 152
+7 	Zlota moneta 	497 911 	902 698
+8 	Helm 	800 493 	1 686 515
+9 	Tusz 	823 576 	1 688 691
+10 	Szkatulka 	552 202 	1 056 157
+11 	Noz 	323 618 	677 562
+12 	Dlugi miecz 	382 846 	833 132
+13 	Maska 	44 676 	99 192
+14 	Naszyjnik 	169 738 	376 418
+15 	Opalowa zawieszka 	610 876 	1 253 986
+16 	Perly 	854 190 	1 853 562
+17 	Kolczan 	671 123 	1 320 297
+18 	Rubinowy pierscien 	698 180 	1 301 637
+19 	Srebrna bransoletka 	446 517 	859 835
+20 	Czasomierz 	909 620 	1 677 534
+21 	Mundur 	904 818 	1 910 501
+22 	Trucizna 	730 061 	1 528 646
+23 	Welniany szal 	931 932 	1 827 477
+24 	Kusza 	952 360 	2 068 204
+25 	Stara ksiega 	926 023 	1 746 556
+26 	Puchar z cynku 	978 724 	2 100 851'''
+
+
+def numberify(string):
+    return int(string.replace(' ', ''))
+
+
+xd = xd.split('\n')
+xd = list(map(lambda x: x.split('	'), xd))
+for wiersz in xd:
+    print(
+        f"('{wiersz[1].strip()}', {numberify(wiersz[2])}, {numberify(wiersz[3])}),")
---
+++ items.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+NAME = 0
+WEIGHT = 1
+VALUE = 2
+
+items = np.array([
+    ('Toporek', 32252, 68674),
+    ('Moneta z brazu', 225790, 471010),
+    ('Korona', 468164, 944620),
+    ('Diamentowy posazek', 489494, 962094),
+    ('Szmaragdowy pas', 35384, 78344),
+    ('Skamieliny', 265590, 579152),
+    ('Zlota moneta', 497911, 902698),
+    ('Helm', 800493, 1686515),
+    ('Tusz', 823576, 1688691),
+    ('Szkatulka', 552202, 1056157),
+    ('Noz', 323618, 677562),
+    ('Dlugi miecz', 382846, 833132),
+    ('Maska', 44676, 99192),
+    ('Naszyjnik', 169738, 376418),
+    ('Opalowa zawieszka', 610876, 1253986),
+    ('Perly', 854190, 1853562),
+    ('Kolczan', 671123, 1320297),
+    ('Rubinowy pierscien', 698180, 1301637),
+    ('Srebrna bransoletka', 446517, 859835),
+    ('Czasomierz', 909620, 1677534),
+    ('Mundur', 904818, 1910501),
+    ('Trucizna', 730061, 1528646),
+    ('Welniany szal', 931932, 1827477),
+    ('Kusza', 952360, 2068204),
+    ('Stara ksiega', 926023, 1746556),
+    ('Puchar z cynku', 978724, 2100851),
+])
